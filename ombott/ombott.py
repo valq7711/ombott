68d086943bd0dc2f6810f89abcdf4f295f4349b7
valq7711 <valq7711@gmail.com>
2024-08-07 01:36:25 +0300

    bump version 2.2

--- ombott/ombott.py
+++ ombott/ombott.py
@@ -18,7 +18,7 @@ from .response import Response, HTTPResponse, HTTPError
 from . import server_adapters
 from . import error_render
 
-__version__ = "2.1"
+__version__ = "2.2"
 
 HTTP_METHODS = 'DELETE GET HEAD OPTIONS PATCH POST PUT'.split()
 
